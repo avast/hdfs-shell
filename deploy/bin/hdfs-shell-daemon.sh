f2091754e3ff4c9cf7398e7f7230ba2b8670495b
Vitasek <vitasek@avast.com>
2017-08-25 07:56:57 +0200

    updated launch scripts

--- deploy/bin/hdfs-shell-daemon.sh
+++ deploy/bin/hdfs-shell-daemon.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-CWD=$(cd $(dirname $0); pwd)  
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+HADOOP_DIR=${HADOOP_CONF_DIR:-/etc/hadoop/conf}
 
-java -Ddaemon=true -Xms200m -Xmx400m -cp ${CWD}/lib/*:/etc/hadoop/conf com.avast.server.hdfsshell.MainApp "$@"
+echo "Launching HDFS Shell Daemon with HADOOP_CONF_DIR: ${HADOOP_DIR}"
+
+java -Ddaemon=true -Xms200m -Xmx400m -cp ${SCRIPT_DIR}/../lib/*:${HADOOP_DIR} com.avast.server.hdfsshell.MainApp "$@"
