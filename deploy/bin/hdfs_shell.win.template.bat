2c61a509982d81f4dd518664c604d39326431cb0
Jonathan Mercier <jmercier@edge1.ad.baikal.cnrgh.fr>
2019-08-30 10:10:41 +0200

    use windows separator

--- deploy/bin/hdfs_shell.win.template.bat
+++ deploy/bin/hdfs_shell.win.template.bat
@@ -8,5 +8,5 @@ set APP_HOME=%DIRNAME%..
 set LIB_DIR=%APP_HOME%/lib
 
 echo Launching HDFS Shell with HADOOP_CONF_DIR: %HADOOP_DIR%
-java.exe -Xms200m -Xmx400m -cp "%LIB_DIR%/*:%HADOOP_DIR%:%HADOOP_CLASSPATH%" ${mainClassName} %CMD_LINE_ARGS%
+java.exe -Xms200m -Xmx400m -cp "%LIB_DIR%/*;%HADOOP_DIR%:%HADOOP_CLASSPATH%" ${mainClassName} %CMD_LINE_ARGS%
 if "%OS%"=="Windows_NT" endlocal
